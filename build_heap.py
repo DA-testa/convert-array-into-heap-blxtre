0f800a356f343447bd282d54a1b2d15837c8d9c6
blxtre <127733096+blxtre@users.noreply.github.com>
2023-03-20 13:36:57 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,37 +1,41 @@
-# python3
-
-
-import math
-
-def sift_down(data, i, swaps):
-    min_index = i
-    n = len(data)
-    l = 2 * i + 1
-    if l < n and data[l] < data[min_index]:
-        min_index = l
-    r = 2 * i + 2
-    if r < n and data[r] < data[min_index]:
-        min_index = r
-    if i != min_index:
-        data[i], data[min_index] = data[min_index], data[i]
-        swaps.append((i, min_index))
-        sift_down(data, min_index, swaps)
-
 def build_heap(data):
     swaps = []
     n = len(data)
-    for i in range(math.floor(n/2), -1, -1):
-        sift_down(data, i, swaps)
+    for i in range(n//2, -1, -1):
+        swaps += sift_down(data, i, swaps)
+    return swaps
+
+def sift_down(data, i, swaps):
+    n = len(data)
+    min_index = i
+    left_child = 2*i+1
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2*i+2
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps += sift_down(data, min_index, swaps)
     return swaps
 
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
+    input_type = input().strip()
+    if input_type == '0':
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    else:
+        # input from file
+        with open(input_type, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+
     assert len(data) == n
+
     swaps = build_heap(data)
+
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-if __name__ == "__main__":
-    main()
