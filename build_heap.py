eabc5710bcb2cc44b67fff3d931b973bf4e50f2e
blxtre <127733096+blxtre@users.noreply.github.com>
2023-03-20 13:54:25 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,36 +1,40 @@
 def build_heap(data):
-    swaps = []
-    n = len(data)
-    for i in range(n//2, -1, -1):
-        swaps += sift_down(data, i, swaps)
-    return swaps
+swaps = []
+# build a heap bottom-up by iterating over all non-leaf nodes in the array
+for i in range(len(data) // 2 - 1, -1, -1):
+# heapify the current node downwards
+swaps = heapify_down(data, i, swaps)
+return swaps
 
-def sift_down(data, i, swaps):
-    n = len(data)
-    min_index = i
-    left_child = 2*i+1
-    if left_child < n and data[left_child] < data[min_index]:
-        min_index = left_child
-    right_child = 2*i+2
-    if right_child < n and data[right_child] < data[min_index]:
-        min_index = right_child
-    if i != min_index:
-        swaps.append((i, min_index))
-        data[i], data[min_index] = data[min_index], data[i]
-        swaps += sift_down(data, min_index, swaps)
-    return swaps
+def heapify_down(data, i, swaps):
+# get the indices of the children nodes
+left = 2 * i + 1
+right = 2 * i + 2
+# initialize the index of the minimum value among the node and its children
+min_index = i
+# compare the node with its left child and update the minimum index if necessary
+if left < len(data) and data[left] < data[min_index]:
+min_index = left
+# compare the node with its right child and update the minimum index if necessary
+if right < len(data) and data[right] < data[min_index]:
+min_index = right
+# if the node is not the minimum, swap it with the minimum and heapify down the swapped node
+if i != min_index:
+swaps.append((i, min_index))
+data[i], data[min_index] = data[min_index], data[i]
+swaps = heapify_down(data, min_index, swaps)
+return swaps
 
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
+# read the input
+n = int(input())
+data = list(map(int, input().split()))
+# build the heap and get the swaps performed
+swaps = build_heap(data)
+# output the swaps
+print(len(swaps))
+for i, j in swaps:
+print(i, j)
 
-    assert len(data) == n
-
-    swaps = build_heap(data)
-
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
-if __name__ == "__main__":
-    main()
+if name == "main":
+main()
