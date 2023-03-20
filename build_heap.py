3ac7f361373f920040e088760c5549f881f7f0e4
blxtre <127733096+blxtre@users.noreply.github.com>
2023-03-20 13:19:20 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,50 +1,37 @@
 # python3
 
 
+import math
+
+def sift_down(data, i, swaps):
+    min_index = i
+    n = len(data)
+    l = 2 * i + 1
+    if l < n and data[l] < data[min_index]:
+        min_index = l
+    r = 2 * i + 2
+    if r < n and data[r] < data[min_index]:
+        min_index = r
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(data, min_index, swaps)
+
 def build_heap(data):
     swaps = []
     n = len(data)
-
-    
-    def sift_down(i):
-        min_index = i
-        left_child = 2*i + 1
-        right_child = 2*i + 2
-
-        if left_child < n and data[left_child] < data[min_index]:
-            min_index = left_child
-
-        if right_child < n and data[right_child] < data[min_index]:
-            min_index = right_child
-
-        if i != min_index:
-            data[i], data[min_index] = data[min_index], data[i]
-            swaps.append((i, min_index))
-            sift_down(min_index)
-
-    
-    for i in range(n//2, -1, -1):
-        sift_down(i)
-
+    for i in range(math.floor(n/2), -1, -1):
+        sift_down(data, i, swaps)
     return swaps
-
 
 def main():
     n = int(input())
     data = list(map(int, input().split()))
-
-    
     assert len(data) == n
-
     swaps = build_heap(data)
-
-    
-    assert len(swaps) <= 4*n
-
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
