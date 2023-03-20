8221473724f878789a7288c5ab5055746750e43a
blxtre <127733096+blxtre@users.noreply.github.com>
2023-03-20 13:14:14 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,36 +3,44 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    n = len(data)
 
+    
+    def sift_down(i):
+        min_index = i
+        left_child = 2*i + 1
+        right_child = 2*i + 2
+
+        if left_child < n and data[left_child] < data[min_index]:
+            min_index = left_child
+
+        if right_child < n and data[right_child] < data[min_index]:
+            min_index = right_child
+
+        if i != min_index:
+            data[i], data[min_index] = data[min_index], data[i]
+            swaps.append((i, min_index))
+            sift_down(min_index)
+
+    
+    for i in range(n//2, -1, -1):
+        sift_down(i)
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
+    
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+    
+    assert len(swaps) <= 4*n
 
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
